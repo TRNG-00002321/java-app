e8d6e65ef3340d829d0395f18834bc506e901e08
jasdhir <jasdhir.training@gmail.com>
2026-01-08 19:08:15 -0600

    java-app

---
+++ src/main/java/com/revature/jen/Calculator.java
@@ -0,0 +1,13 @@
+package com.revature.jen;
+
+public class Calculator {
+    /**
+     * Adds two integers.
+     * @param a first operand
+     * @param b second operand
+     * @return sum of a and b
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
